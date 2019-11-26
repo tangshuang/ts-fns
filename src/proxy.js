304900478bb5d536767a7b9cfdc820d46029eeeb
tangshuang <mail@tangshuang.net>
2019-11-26 10:29:29 +0800

    fix proxy

--- src/proxy.js
+++ src/proxy.js
@@ -1,66 +1,77 @@
 import { makeKeyPath } from './key-path.js'
 import { isFunction, isObject, isArray } from './is.js'
 
-export function createProxy(obj, options = {}) {
+const PROXY_TARGET = Symbol.for('[[Target]]')
+
+const createProxyObject = (obj, options = {}, parents = []) => {
   const { get, set, del } = options
-  const createProxy = (obj, parents = []) => {
-    const handler = {
-      get(target, key, receiver) {
-        if (key === Symbol.for('[[Target]]')) {
-          return target
-        }
+  const handler = {
+    get(target, key, receiver) {
+      if (key === PROXY_TARGET) {
+        return target
+      }
 
-        const value = Reflect.get(target, key, receiver)
-        let use
+      const value = Reflect.get(target, key, receiver)
+      let use
 
-        if (isObject(value) || isArray(value)) {
-          const proxy = createProxy(value, [ ...parents, key ])
-          use = proxy
-        }
-        else {
-          use = value
-        }
+      if (isObject(value) || isArray(value)) {
+        const proxy = createProxyObject(value, options, [ ...parents, key ])
+        use = proxy
+      }
+      else {
+        use = value
+      }
 
-        if (isFunction(get)) {
-          const chain = [ ...parents, key ]
-          const keyPath = makeKeyPath(chain)
-          return get([obj, keyPath, use], [target, key, value], receiver)
+      if (isFunction(get)) {
+        const chain = [ ...parents, key ]
+        const keyPath = makeKeyPath(chain)
+        return get([obj, keyPath, use], [target, key, value], receiver)
+      }
+      else {
+        return use
+      }
+    },
+    set(target, key, value, receiver) {
+      // return original data if value is a proxy
+      const v = value && typeof value === 'object' ? getProxied(value) : value
+
+      if (isFunction(set)) {
+        const chain = [ ...parents, key ]
+        const keyPath = makeKeyPath(chain)
+        const needto = set([obj, keyPath, v], [target, key, v], receiver)
+        if (needto) {
+          Reflect.set(target, key, v, receiver)
         }
-        else {
-          return use
+        return true
+      }
+      else {
+        return Reflect.set(target, key, v, receiver)
+      }
+    },
+    deleteProperty(target, key) {
+      if (isFunction(del)) {
+        const chain = [ ...parents, key ]
+        const keyPath = makeKeyPath(chain)
+        const needto = del([obj, keyPath], [target, key])
+        if (needto) {
+          Reflect.deleteProperty(target, key)
         }
-      },
-      set(target, key, value, receiver) {
-        if (isFunction(set)) {
-          const chain = [ ...parents, key ]
-          const keyPath = makeKeyPath(chain)
-          const needto = set([obj, keyPath, value], [target, key, value], receiver)
-          if (needto) {
-            target[key] = value
-          }
-          return true
-        }
-        else {
-          return Reflect.set(target, key, value, receiver)
-        }
-      },
-      deleteProperty(target, key) {
-        if (isFunction(del)) {
-          const chain = [ ...parents, key ]
-          const keyPath = makeKeyPath(chain)
-          const needto = del([obj, keyPath], [target, key])
-          if (needto) {
-            target[key] = value
-          }
-          return true
-        }
-        else {
-          return Reflect.deleteProperty(target, key)
-        }
-      },
-    }
-    return new Proxy(obj, handler)
+        return true
+      }
+      else {
+        return Reflect.deleteProperty(target, key)
+      }
+    },
   }
-  const proxy = createProxy(obj)
+  return new Proxy(obj, handler)
+}
+
+export function createProxy(obj, options = {}) {
+  const proxy = createProxyObject(obj, options)
   return proxy
+}
+
+export function getProxied(value) {
+  const target = value[PROXY_TARGET] ? value[PROXY_TARGET] : value
+  return target
 }
