c0badeae5ed766fefdbfa9189079a7de2669b1cb
tangshuang <mail@tangshuang.net>
2019-11-26 22:37:04 +0800

    fix proxy.get

--- es/proxy.js
+++ es/proxy.js
@@ -12,27 +12,27 @@ const createProxyObject = (obj, options = {}, parents = []) => {
   const handler = {
     get(target, key, receiver) {
       if (key === PROXY_TARGET) {
-        return target
+        return getProxied(target)
       }
 
-      const value = Reflect.get(target, key, receiver)
-      let use
-
-      if (isObject(value) || isArray(value)) {
-        const proxy = createProxyObject(value, options, [ ...parents, key ])
-        use = proxy
-      }
-      else {
-        use = value
-      }
+      let value
 
       if (isFunction(get)) {
         const chain = [ ...parents, key ]
         const keyPath = makeKeyPath(chain)
-        return get([obj, keyPath, use], [target, key, value], receiver)
+        value = get([obj, keyPath, receiver], [target, key, receiver])
       }
       else {
-        return use
+        value = Reflect.get(target, key, receiver)
+      }
+
+      // if sub value is an object, return an proxy again
+      if ((isObject(value) || isArray(value)) && !value[PROXY_TARGET]) {
+        const proxy = createProxyObject(value, options, [ ...parents, key ])
+        return proxy
+      }
+      else {
+        return value
       }
     },
     set(target, key, value, receiver) {
@@ -42,7 +42,7 @@ const createProxyObject = (obj, options = {}, parents = []) => {
       if (isFunction(set)) {
         const chain = [ ...parents, key ]
         const keyPath = makeKeyPath(chain)
-        const needto = set([obj, keyPath, v], [target, key, v], receiver)
+        const needto = set([obj, keyPath, v, receiver], [target, key, v, receiver])
         if (needto) {
           Reflect.set(target, key, v, receiver)
         }
@@ -77,7 +77,11 @@ export function createProxy(obj, options = {}) {
 }
 
 /** */
-export function getProxied(value) {
-  const target = value[PROXY_TARGET] ? value[PROXY_TARGET] : value
+export function getProxied(proxy) {
+  const target = proxy[PROXY_TARGET] ? proxy[PROXY_TARGET] : proxy
+  // make sure to return original object
+  if (target[PROXY_TARGET]) {
+    return getProxied(target)
+  }
   return target
 }
