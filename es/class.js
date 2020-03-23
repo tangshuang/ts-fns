1bc8875052eca3468706edc11ca7b4a01181b2e0
tangshuang <mail@tangshuang.net>
2020-03-23 23:32:51 +0800

    update

--- es/class.js
+++ es/class.js
@@ -3,6 +3,26 @@
  */
 
 /** */
-export function getConstructor(ins) {
+export function constructof(ins) {
   return Object.getPrototypeOf(ins).constructor
+}
+
+/** */
+export function inherit(Class, Parent) {
+  return Object.setPrototypeOf(Class, Parent)
+}
+
+/** */
+export function extend(Class, proptotypes, statics) {
+  const { name } = Class
+  const o = {
+    [name]: class extends Class {},
+  }
+  const C = o[name]
+
+  Object.assign(C.prototype, proptotypes || {})
+  Object.assign(C, { ...Class })
+  Object.assign(C, statics || {})
+
+  return C
 }
