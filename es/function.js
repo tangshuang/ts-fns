017ea62e7218b11ea8a6a72e78104a3dedf93b8c
tangshuang <mail@tangshuang.net>
2020-04-25 22:26:36 +0800

    update function module

--- es/function.js
+++ es/function.js
@@ -2,32 +2,69 @@
  * @module function
  */
 
-import { getObjectHash } from './object.js'
+import { getObjectHash, each } from './object.js'
 
-/** */
-export function compute_(fn, expire = 60000) {
-  const cache = {}
+/**
+ * Create a function which cache result by parameters.
+ * Notice that, there is an async loop to clear expired cache every 1s. You can use .recycle(0) to stop the loop.
+ * @param {function} fn the original function to calculate result
+ * @param {number} expire the time to expire cache, if set 0 (or not set), the cache will never be expired, default 0.
+ * @return {function} you can call a .clear() method on this function to clear the cache
+ * @example const compute = compute_((x, y) => {
+ *   return x + y
+ * })
+ *
+ * // the following two computing will call original fn only once
+ * const a = compute(1, 3)
+ * const b = compute(1, 3)
+ *
+ * compute.clear() // clear expired cache
+ * compute.clear(true) // force clear all cache
+ *
+ * compute.recycle(0) // stop the recycle task
+ * compute.recycle(500) // start a task to recycle cache to avoid memory overflow
+ */
+export function compute_(fn, expire = 0) {
+  let cache = {}
+  let timer = null
 
-  const recycle = () => {
-    const keys = Object.keys(cache)
-    keys.forEach((key) => {
-      const { time } = cache[key]
+  const clear = (force) => {
+    if (force) {
+      cache = {}
+      return
+    }
+
+    if (!expire) {
+      return
+    }
+
+    each(cache, ({ time }) => {
       if (time + expire <= Date.now()) {
         delete cache[key]
       }
     })
-    setTimeout(recycle, 1000)
   }
 
-  if (expire > 0) {
-    recycle()
+  const recycle = (interval = 1000) => {
+    clear()
+    clearTimeout(timer)
+
+    if (interval > 0) {
+      timer = setTimeout(recycle, interval)
+    }
   }
 
-  return function(...args) {
+  recycle()
+
+  const compute = function(...args) {
     const hash = getObjectHash(args)
+
     if (hash in cache) {
       const item = cache[hash]
-      return item.result
+      const { time, result } = item
+      if (!expire || (time + expire > Date.now())) {
+        return result
+      }
     }
 
     const result = fn.apply(this, args)
@@ -36,51 +73,146 @@ export function compute_(fn, expire = 60000) {
 
     return result
   }
+
+  compute.clear = clear
+  compute.recycle = recycle
+
+  return compute
 }
 
-/** */
-export function get_(fn) {
+/**
+ * create a getter function which will cache the result, cache will be released automaticly
+ * @param {function} fn the getter function, notice, without parameters
+ * @param {number} type the type of automatic releasing, default 1.
+ *  - 0: never released
+ *  - 1: in Promise microtask
+ *  - 2: in timeout task
+ *  - 3: in requestAnimationFrame
+ *  - 4: in requestIdleCallback
+ * @return {function} you can call .clear() to clear cache immediately
+ * @example const get = get_(() => {
+ *   return Math.random()
+ * })
+ *
+ * // the following two getting will call original fn only once
+ * const a = get()
+ * const b = get()
+ * a === b
+ *
+ * // type: 1
+ * const getter = get_(() => Date.now(), 1)
+ * const e = getter()
+ * Promise.then(() => {
+ *   const h = getter()
+ *   e !== h // cache is released in a previous Promise microtask
+ * })
+ * setTimeout(() => {
+ *   const f = getter()
+ *   e !== f // when type is 1, the cache will be release in a Promise microtask, so when we call setTimeout, cache is gone
+ * })
+ *
+ * // type: 2
+ * const use = get_(() => Date.now(), 2)
+ * const m = use()
+ * Promise.then(() => {
+ *   const n = use()
+ *   m === n // when type is 2, the cache will be release in a setTimeout task, so when we call in a Promise.then, cache is existing
+ * })
+ * setTimeout(() => {
+ *   const l = use()
+ *   m !== l // cache was released in a previous setTimeout task
+ * })
+ */
+export function get_(fn, type = 1) {
   let iscalling = false
   let cache = null
 
-  return function() {
+  const clear = () => {
+    iscalling = false
+    cache = null
+  }
+
+  const recycle = () => {
+    if (type === 1) {
+      Promise.resolve().then(clear)
+    }
+    else if (type === 2) {
+      setTimeout(clear, 0)
+    }
+    else if (type === 3) {
+      requestAnimationFrame(clear)
+    }
+    else if (type === 4) {
+      requestIdleCallback(clear)
+    }
+  }
+
+  const get = function() {
     if (iscalling) {
       return cache
     }
 
-    Promise.resolve().then(() => {
-      iscalling = false
-      cache = null
-    })
-
-    iscalling = true
+    recycle()
 
     const result = fn.call(this)
+
+    iscalling = true
     cache = result
+
     return result
   }
+
+  get.clear = clear
+
+  return get
 }
 
-/** */
+/**
+ * Create a function which return a Promise and cached by parameters.
+ * Memory recycle is the same as compute_
+ * @param {function} fn a function, can be async function or normal function
+ * @param {number} expire the expire time for releasing cache
+ * @return {function} .clear() and .recycle() is available
+ * @example const fn = async_(async () => {})
+ *
+ * const a = fn()
+ * const b = fn()
+ *
+ * a === b // the same Promise
+ */
 export function async_(fn, expire = 0) {
-  const cache = {}
+  let cache = {}
+  let timer = null
 
-  const recycle = () => {
-    const keys = Object.keys(cache)
-    keys.forEach((key) => {
-      const { time } = cache[key]
+  const clear = (force) => {
+    if (force) {
+      cache = {}
+      return
+    }
+
+    if (!expire) {
+      return
+    }
+
+    each(cache, ({ time }) => {
       if (time + expire <= Date.now()) {
         delete cache[key]
       }
     })
-    setTimeout(recycle, 1000)
   }
 
-  if (expire > 0) {
-    recycle()
+  const recycle = (interval = 1000) => {
+    clear()
+    clearTimeout(timer)
+
+    if (interval > 0) {
+      timer = setTimeout(recycle, interval)
+    }
   }
 
-  return function(...args) {
+  recycle()
+
+  const asyncFn = function(...args) {
     const hash = getObjectHash(args)
     if (hash in cache) {
       const item = cache[hash]
@@ -100,40 +232,107 @@ export function async_(fn, expire = 0) {
 
     return deferer
   }
+
+  asyncFn.clear = clear
+  asyncFn.recycle = recycle
+
+  return asyncFn
 }
 
-/** */
-export function invoke_(fn, count = 1) {
-  const cache = {}
+/**
+ * create a function whose result will be cached, and the cache will be released by invoke count
+ * @param {function} fn
+ * @param {number} count
+ * @param {number} expire the expire time after latest invoke
+ * @return {function} .clear() and .recycle() is avaliable, recycle runs in a loop task which run every minutes.
+ * @example const invoke = invoke_(() => {
+ *   return Math.random()
+ * }, 2)
+ *
+ * const a = invoke()
+ * const b = invoke()
+ * const c = invoke()
+ *
+ * a === b
+ * a !== c
+ */
+export function invoke_(fn, count = 1, expire = 0) {
+  let cache = {}
+  let timer = null
 
-  return function(...args) {
+  const clear = (force) => {
+    if (force) {
+      cache = {}
+      return
+    }
+
+    if (!expire) {
+      return
+    }
+
+    each(cache, ({ time }) => {
+      if (time + expire <= Date.now()) {
+        delete cache[key]
+      }
+    })
+  }
+
+  const recycle = (interval = 1000) => {
+    clear()
+    clearTimeout(timer)
+
+    if (interval > 0) {
+      timer = setTimeout(recycle, interval)
+    }
+  }
+
+  recycle()
+
+  const invoke = function(...args) {
     const hash = getObjectHash(args)
     const item = cache[hash]
 
     if (item) {
-      if (item.invoked > count) {
+      if (item.invoked >= count) {
         delete cache[hash]
       }
       else {
+        item.time = Date.now()
         item.invoked ++
         return item.result
       }
     }
 
     const result = fn.apply(this, args)
-    cache[hash] = { result, invoked: 1 }
+    const time = Date.now()
+    cache[hash] = { result, invoked: 1, time }
 
     return result
   }
+
+  invoke.clear = clear
+  invoke.recycle = recycle
+
+  return invoke
 }
 
-/** */
+/**
+ * @example const pipe = pipe_(
+ *   x => x + 1,
+ *   x => x - 1,
+ *   x => x * x,
+ *   x => x / x,
+ * )
+ *
+ * const y = pipe(10) // 10
+ */
 export function pipe_(...fns) {
-  return (...args) => {
-    let result = fns.shift()(...args)
-    for (let i = 0, len = fns.length; i < len; i ++) {
-      const fn = fns[i]
-      result = fn(result)
+  const funcs = fns.filter(fn => typeof fn === 'function')
+  return function(arg) {
+    let result = arg
+    for (let i = 0, len = funcs.length; i < len; i ++) {
+      const fn = funcs[i]
+      result = fn.call(this, result)
     }
     return result
   }
---
+++ test/function.test.js
@@ -0,0 +1,190 @@
+import { compute_, get_, async_, invoke_, pipe_ } from '../es/index.js'
+
+describe('function', () => {
+  test('compute_: basic', () => {
+    let count = 0
+
+    const compute = compute_((x, y) => {
+      count ++
+      return x + y
+    })
+
+    const a = compute(1, 2)
+    const b = compute(1, 2)
+    const c = compute(1, 3)
+    const d = compute(1, 3)
+
+    expect(a).toBe(b)
+    expect(c).toBe(d)
+    expect(count).toBe(2)
+
+    const e = compute(1, 3)
+    compute.clear(true) // clear all cache
+    const f = compute(1, 3) // recompute
+
+    expect(e).toBe(c)
+    expect(e).toBe(f)
+    expect(count).toBe(3)
+  })
+
+  test('compute_: expired', (done) => {
+    let count = 0
+
+    const compute = compute_((x, y) => {
+      count ++
+      return x + y
+    }, 8)
+
+    const a = compute(1, 2)
+    const b = compute(1, 2)
+    const c = compute(1, 2)
+
+    expect(count).toBe(1)
+
+    setTimeout(() => {
+      const d = compute(1, 2)
+      // cache not expired
+      expect(count).toBe(1)
+    }, 4)
+
+    setTimeout(() => {
+      const e = compute(1, 2)
+      // cache expired
+      expect(count).toBe(2)
+      done()
+    }, 8)
+  })
+
+  test('get_: basic', () => {
+    let count = 0
+
+    const get = get_(() => {
+      count ++
+      return Math.random()
+    })
+
+    const a = get()
+    const b = get()
+
+    expect(a).toBe(b)
+    expect(count).toBe(1)
+  })
+
+  test('get_: Promise', (done) => {
+    let count = 0
+
+    const get = get_(() => {
+      count ++
+      return Math.random()
+    }, 1)
+
+    const a = get()
+    const b = get()
+
+    expect(a).toBe(b)
+    expect(count).toBe(1)
+
+    Promise.resolve().then(() => {
+      const c = get()
+
+      expect(c).not.toBe(a)
+      expect(count).toBe(2)
+
+      setTimeout(() => {
+        const d = get()
+
+        expect(d).not.toBe(a)
+        expect(count).toBe(3)
+
+        done()
+      })
+    })
+  })
+
+  test('get_: timeout', (done) => {
+    let count = 0
+
+    const get = get_(() => {
+      count ++
+      return Math.random()
+    }, 2)
+
+    const a = get()
+    const b = get()
+
+    expect(a).toBe(b)
+    expect(count).toBe(1)
+
+    Promise.resolve().then(() => {
+      const c = get()
+
+      expect(c).toBe(a)
+      expect(count).toBe(1)
+
+      setTimeout(() => {
+        const d = get()
+
+        expect(d).not.toBe(a)
+        expect(count).toBe(2)
+
+        done()
+      })
+    })
+  })
+
+  test('asnyc_: basic', (done) => {
+    let count = 0
+
+    const afn = async_((x, y) => {
+      count ++
+      return x + y
+    })
+
+    const p1 = afn(1, 2)
+    const p2 = afn(1, 2)
+
+    expect(p1).toBe(p2)
+    expect(count).toBe(0) // not invoked
+
+    Promise.all([p1, p2]).then(([z1, z2]) => {
+      expect(z1).toBe(z2)
+      expect(count).toBe(1) // invoked async
+      done()
+    })
+  })
+
+  test('invoke_: basic', () => {
+    let count = 0
+
+    const fn = invoke_(() => {
+      count ++
+      return Math.random()
+    }, 2)
+
+    const a = fn()
+
+    expect(count).toBe(1)
+
+    const b = fn()
+
+    expect(count).toBe(1)
+    expect(a).toBe(b)
+
+    const c = fn()
+
+    expect(count).toBe(2)
+    expect(c).not.toBe(a)
+  })
+
+  test('pipe_', () => {
+    const fn = pipe_(
+      x => x + 2,
+      x => x - 1,
+      x => x * 3,
+    )
+
+    const y = fn(2)
+
+    expect(y).toBe(9)
+  })
+})
