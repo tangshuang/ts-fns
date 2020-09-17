3694d27139199f6187beb6f1c8b4490a89f31d0d
tangshuang <tisontang@tencent.com>
2020-09-17 17:07:04 +0800

    reset each

--- es/object.js
+++ es/object.js
@@ -307,8 +307,8 @@ export function each(obj, fn, descriptor = false) {
     const keys = Object.keys(descriptors)
     keys.forEach((key) => {
       const descriptor = descriptors[key]
-      const { get, set, enumerable, writable } = descriptor
-      if (enumerable || (get || set) || ('value' in descriptor && writable)) {
+      const { get, set, enumerable, configurable, writable } = descriptor
+      if (enumerable || (get || set) || (configurable && writable)) {
         fn(descriptor, key, obj)
       }
     })
