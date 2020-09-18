5fd976f1ade37be56e576aad7ff0db747e18d369
tangshuang <tisontang@tencent.com>
2020-09-18 11:29:27 +0800

    update isFunction and isConstructor

---
+++ test/is.test.js
@@ -0,0 +1,36 @@
+import { isFunction, isConstructor } from '../es/index.js'
+
+describe('is', () => {
+  test('isFunction & isConstructor', () => {
+    function test() {}
+    expect(isFunction(test)).toBe(true)
+    expect(isConstructor(test)).toBe(false)
+
+    const sy = test.bind(null)
+    expect(isFunction(sy)).toBe(true)
+    expect(isConstructor(sy)).toBe(false)
+
+    const mn = () => {}
+    expect(isFunction(mn)).toBe(true)
+    expect(isConstructor(mn)).toBe(false)
+
+    expect(isFunction(String)).toBe(false)
+    expect(isConstructor(String)).toBe(true)
+
+    function Some() {
+      this.name = 'some'
+    }
+    expect(isFunction(Some)).toBe(false)
+    expect(isConstructor(Some)).toBe(true)
+
+    const xs = () => {
+      this.xx = null
+    }
+    expect(isFunction(xs)).toBe(true)
+    expect(isConstructor(xs)).toBe(false)
+
+    const Tx = class {}
+    expect(isFunction(Tx)).toBe(false)
+    expect(isConstructor(Tx)).toBe(true)
+  })
+})
